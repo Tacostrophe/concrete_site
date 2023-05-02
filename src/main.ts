4b2627ec63478252a1068aa682d2ed1af120f90c
Дмитрий Кутин <TacoKong@yandex.ru>
2023-05-02 17:12:10 +0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function start() {
   const PORT = process.env.PORT ?? 5000;
   const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('sprint_3')
-  //   .setDescription('')
-  //   .setVersion('1.0.0')
-  //   .addTag('пользователи')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api/docs', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('concrete_site')
+    .setDescription(
+      'Документооборот строительной площадки, ориентированный на бетон',
+    )
+    .setVersion('1.0.0')
+    // .addTag('пользователи')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 }
