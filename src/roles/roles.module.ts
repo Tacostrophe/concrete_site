427381c07a34274d062edb82675bc8bcfc431e63
Дмитрий Кутин <TacoKong@yandex.ru>
2023-05-06 08:40:03 +0300

    added user and exported service

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -3,10 +3,13 @@ import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from './roles.model';
+import { User } from 'src/users/users.model';
+import { UserRole } from 'src/users/user-role.model';
 
 @Module({
   controllers: [RolesController],
   providers: [RolesService],
-  imports: [SequelizeModule.forFeature([Role])],
+  imports: [SequelizeModule.forFeature([Role, User, UserRole])],
+  exports: [RolesService],
 })
 export class RolesModule {}
