2630c6f58aceacc0cd43ec9d95536954b615de92
Дмитрий Кутин <TacoKong@yandex.ru>
2023-05-11 08:49:50 +0300

    создан guard для ролей

---
+++ src/auth/auth-roles.guard.ts
@@ -0,0 +1,46 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from './auth-roles.decorator';
+import { User } from 'src/users/users.model';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      const requiredRoles = this.reflector.getAllAndOverride<string[]>(
+        ROLES_KEY,
+        [context.getHandler(), context.getClass()],
+      );
+
+      if (!requiredRoles) {
+        return true;
+      }
+
+      const req = context.switchToHttp().getRequest();
+      const authHeader = req.headers.authorization;
+      const [bearer, token] = authHeader.split(' ');
+
+      if (bearer !== 'Bearer' || !token) {
+        throw new UnauthorizedException({ message: 'Неверный токен' });
+      }
+      const user: User = this.jwtService.verify(token);
+      req.user = user;
+      return user.roles.some((role) => requiredRoles.includes(role.value));
+    } catch (error) {
+      throw new HttpException('Нет доступа', HttpStatus.FORBIDDEN);
+    }
+  }
+}
