2b1d94782d398d6484006e1a197d21927150b767
Дмитрий Кутин <TacoKong@yandex.ru>
2023-05-11 08:47:03 +0300

    добавлены guardы, удаление роли, удаление пользователя

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Headers,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AuthJwtGuard } from 'src/auth/auth-jwt.guard';
+import { Roles } from 'src/auth/auth-roles.decorator';
+import { RolesGuard } from 'src/auth/auth-roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
 
 @ApiTags('пользователи')
 @Controller('users')
@@ -12,6 +24,8 @@ export class UsersController {
 
   @ApiOperation({ summary: 'создание пользователя' })
   @ApiResponse({ status: 200, type: User })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
@@ -25,10 +39,48 @@ export class UsersController {
     return this.usersService.listUsers();
   }
 
-  @ApiOperation({ summary: 'получение роли по значение' })
+  @ApiOperation({ summary: 'получение информации о себе' })
   @ApiResponse({ status: 200, type: User })
+  @UseGuards(AuthJwtGuard)
+  @Get('/me')
+  me(@Headers('authorization') authorization: string) {
+    return this.usersService.retrieveMe(authorization);
+  }
+
+  @ApiOperation({ summary: 'добавление роли' })
+  @ApiResponse({ status: 200 })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
+  @Post('/role')
+  addRole(@Body() roleDto: AddRoleDto) {
+    return this.usersService.addRole(roleDto);
+  }
+
+  @ApiOperation({
+    summary: 'получение пользователя по значению электронной почты',
+  })
+  @ApiResponse({ status: 200, type: User })
+  @UseGuards(AuthJwtGuard)
   @Get('/:email')
   retrieve(@Param('email') email: string) {
     return this.usersService.retrieveUserByEmail(email);
+  }
+
+  @ApiOperation({ summary: 'изъятие роли у пользователя' })
+  @ApiResponse({ status: 200 })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
+  @Delete('/role')
+  deleteRole(@Body() roleDto: AddRoleDto) {
+    return this.usersService.deleteRoleByValue(roleDto);
+  }
+
+  @ApiOperation({ summary: 'удаление пользователя' })
+  @ApiResponse({ status: 200 })
+  @Roles('admin')
+  @UseGuards(RolesGuard)
+  @Delete('/:pk')
+  delete(@Param('pk') pk: number) {
+    return this.usersService.deleteUserByPk(pk);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { CreateUserDto } from './dto/create-user.dto';
 import { RolesService } from 'src/roles/roles.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import { UserRole } from './user-role.model';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel(User) private userRepository: typeof User,
+    @InjectModel(UserRole) private userRoleRepository: typeof UserRole,
     private rolesService: RolesService,
+    private jwtService: JwtService,
   ) {}
 
   async createUser(dto: CreateUserDto) {
@@ -32,5 +37,93 @@ export class UsersService {
       include: { all: true },
     });
     return user;
+  }
+
+  async retrieveMe(authorization: string) {
+    const [, token] = authorization.split(' ');
+    const userFromToken = this.jwtService.verify(token);
+    const user = await this.userRepository.findByPk(userFromToken.id, {
+      include: { all: true },
+    });
+    return user;
+  }
+
+  async deleteUserByPk(id: number) {
+    const user = this.userRepository.findByPk(id);
+    if (!user) {
+      throw new HttpException(
+        'Пользователя с таким id не существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    await User.destroy({ where: { id } });
+  }
+
+  async addRole(dto: AddRoleDto) {
+    const user = await this.userRepository.findOne({
+      where: { id: dto.userId },
+      include: { all: true },
+    });
+    if (!user) {
+      throw new HttpException(
+        'Пользователя с таким id не существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    } else if (user.roles.find((role) => role.value === dto.value)) {
+      throw new HttpException(
+        `Пользователь уже ${dto.value}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const role = await this.rolesService.retrieveRoleByValue(dto.value);
+    if (!role) {
+      throw new HttpException(
+        'Такой роли не существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    await user.$add('roles', role.id);
+    return dto;
+  }
+
+  async deleteRoleByValue(dto: AddRoleDto) {
+    const role = await this.rolesService.retrieveRoleByValue(dto.value);
+    if (!role) {
+      throw new HttpException(
+        'Такой роли не существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    console.log(role);
+    const user = await this.userRepository.findOne({
+      where: { id: dto.userId },
+      include: { all: true },
+    });
+    if (!user) {
+      throw new HttpException(
+        'Такого пользователя не существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    console.log(user);
+    const userRole = await this.userRoleRepository.findOne({
+      where: {
+        userId: dto.userId,
+        roleId: role.id,
+      },
+    });
+    if (!userRole) {
+      throw new HttpException(
+        'Данный пользователь не существует или не обладает такой ролью',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    console.log(userRole);
+    await UserRole.destroy({
+      where: {
+        roleId: role.id,
+        userId: user.id,
+      },
+    });
   }
 }
